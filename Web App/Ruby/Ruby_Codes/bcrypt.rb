4322edf0c31e99224e6f7643d846291d8f623af2
Giovanna Fiorese <102384842+gifiorese@users.noreply.github.com>
2022-07-06 15:42:45 -0300

    Delete Web App/Ruby directory

--- Web App/Ruby/Ruby_Codes/bcrypt.rb
+++
@@ -1,9 +0,0 @@
-require 'bcrypt'
-
-#my_password = BCrypt::Password.create("my password")
-
-#puts my_password
-
-my_password = BCrypt::Password.new("$2a$12$4uETd2TsfPVV9RV/3kxTEe42LAjm7sQni2mLs7.18HcJtqFQqKv8q")
-puts my_password == "my password"     #=> true
-puts my_password == "not my password" #=> false
