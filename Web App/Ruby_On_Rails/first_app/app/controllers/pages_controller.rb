21768b41f7845010d307d8fd64fd441bd0e51675
Giovanna Fiorese <giovannafiorese@gmail.com>
2022-07-07 13:25:18 -0300

    Add About Page

--- Web App/Ruby_On_Rails/first_app/app/controllers/pages_controller.rb
+++ Web App/Ruby_On_Rails/first_app/app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+  #Home Page
   def home
+  end
+
+  #About Page
+  def about
   end
 end
--- Web App/Ruby_On_Rails/first_app/config/routes.rb
+++ Web App/Ruby_On_Rails/first_app/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   #pages Controller with the Method/Action "home"
-  root 'pages#home' 
+  root 'pages#home'
+
+  #A GET Request to the "About" page
+    #Sending the request to the Pages Controller
+  get 'about', to: 'pages#about'
 end
